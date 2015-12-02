30733206f3f497d006fa3216f97315e2ca010b8f
bcronin <bcronin@resonancelabs.com>
2015-12-02 13:20:40 -0800

    Basic instrumentation

--- packages/simple/lib/server/db.js
+++ packages/simple/lib/server/db.js
@@ -10,17 +10,23 @@ Meteor.startup(function() {
     }
 
     traceguide.options({
-        access_token : "{your_access_token}",
-        group_name   : "meteor/simple",
-        debug        : true,
+        access_token   : "{your_access_token}",
+        group_name     : "meteor/simple",
+        debug          : true,
         log_to_console : true,
-        verbosity    : 2,
+        verbosity      : 2,
+        report_period_millis    : 500,
     });
 
     var rollback = [];
+    var failed = false;
     try {
-        instrumentCollection(rollback, Mongo.Collection.prototype);
+        instrumentMethods(rollback, Meteor.default_server.method_handlers);
     } catch (e) {
+        failed = true;
+    }
+
+    if (failed) {
         console.log('Instrumentation failed. Rolling back.');
         _.each(rollback, function(arr) {
             arr[0][arr[1]] = arr[2];
@@ -28,20 +34,14 @@ Meteor.startup(function() {
     }
 });
 
-function instrumentCollection(rollback, proto) {
-    var methods = [
-        'find',
-        'insert',
-        'update',
-        'remove',
-    ];
-    _.each(methods, function(name) {
-        wrapPassThrough(rollback, proto, name);
+function instrumentMethods(rollback, list) {
+    _.each(_.keys(list), function (name) {
+        wrapPassThrough(rollback, list, name, "meteor/methods");
     });
-    console.log('Mongo instrumentation complete');
-}
+    console.log('Method instrumentation complete');
+};
 
-function wrapPassThrough(rollback, proto, name) {
+function wrapPassThrough(rollback, proto, name, prefix) {
     var baseImp = proto[name];
     rollback.push(proto, name, baseImp);
 
@@ -49,9 +49,16 @@ function wrapPassThrough(rollback, proto, name) {
         throw new Error('Prototype does not have a function named:', name);
     }
     proto[name] = function() {
-        var span = traceguide.span("meteor/Mongo.Collection/" + name);
-        var ret = baseImp.apply(this, arguments);
-        span.end();
+        console.log("Call to '" + name + "'");
+        var span = traceguide.span(prefix + "/" + name);
+        span.endUserID(this.userId || "unknown_user");
+
+        var ret;
+        try {
+            ret = baseImp.apply(this, arguments);
+        } finally {
+            span.end();
+        }
         return ret;
     };
 }
